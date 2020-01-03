6d170cf8ab8f424a105b60d239f15b5cecb9daf8
fujimoto kyosuke <kyoro.f@gmail.com>
2020-01-03 11:43:00 +0900

    options
    - use optparse-applicative
    - add n option

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,10 @@
 module Main where
 
+import Data.Monoid
+import Data.Char
+import Text.Read
+import Options.Applicative
+
 lefts :: Int -> String -> String
 lefts n = unlines . map (left n) . lines
 
@@ -9,5 +14,28 @@ left = slice 0
 slice :: Int -> Int -> String -> String
 slice from to = take (to - from) . drop from
 
+data Options = Options { number :: Int } deriving (Show)
+
+numberP :: Parser Int
+numberP = option auto $ mconcat
+    [ short 'n'
+    , long "number"
+    , value 10
+    , metavar "N"
+    , showDefault
+    ]
+
+optionsP :: Parser Options
+optionsP = (<*>) helper $
+    Options <$> numberP
+
+parserInfo :: ParserInfo Options
+parserInfo = info optionsP $ mconcat []
+
+run :: Options -> IO ()
+run (Options n) = interact $ lefts n
+
 main :: IO ()
-main = interact $ lefts 15
+main = do
+    options <- execParser parserInfo
+    run options
